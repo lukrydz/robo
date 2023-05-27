53adf2cf42d01a9b8af030a702ddc936e9f23dec
Magda K-L <magdalenazur86@gmail.com>
2023-05-27 10:52:08 +0200

    first commit

---
+++ src/main/java/com/example/robodog/RobodogApplication.java
@@ -0,0 +1,13 @@
+package com.example.robodog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobodogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobodogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/robodog/RobodogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.robodog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobodogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
